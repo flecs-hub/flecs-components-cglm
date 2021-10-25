7a6f5507adc0a223a728b2f2f75be95b0b101154
Sander Mertens <sander.mertens8@gmail.com>
2021-10-25 12:16:42 -0700

    Initial commit

---
+++ include/flecs_components_cglm.h
@@ -0,0 +1,22 @@
+#ifndef FLECS_COMPONENTS_CGLM_H
+#define FLECS_COMPONENTS_CGLM_H
+
+/* This generated file contains includes for project dependencies */
+#include "flecs-components-cglm/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+FLECS_COMPONENTS_CGLM_API
+extern ECS_COMPONENT_DECLARE(vec3);
+
+FLECS_COMPONENTS_CGLM_API
+void FlecsComponentsCglmImport(
+    ecs_world_t *world);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
